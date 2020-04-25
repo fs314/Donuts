91973641dd191d910c5f958bcf778e53e4ede648
fs314 <flaminia.salustri@hotmail.it>
2020-04-25 19:04:53 +0100

    Added no script tag to display error in case js is disabled

--- js/Donuts.js
+++ js/Donuts.js
@@ -8,7 +8,7 @@ export const createDonut = function(donuts) {
     let radiusTube = Math.random()/ 10; 
     let dntColor = createRandomColor();
  
-    let geometry = new THREE.TorusGeometry(radiusTube, radiusTube, 3, 100);
+    let geometry = new THREE.TorusGeometry(radiusTube, radiusTube, 3, 100, Math.PI * 2);
     let material = new THREE.MeshBasicMaterial({ color: dntColor });
 
     donut = new THREE.Mesh(geometry, material);
