78b705ad71b91b3d2d54ee2a376cb45e4cb9b38d
fs314 <flaminia.salustri@hotmail.it>
2020-05-03 15:56:22 +0100

    first refactor

--- js/LevelHandler.js
+++ js/LevelHandler.js
@@ -1,47 +1,28 @@
 const configuration = {
-    container:  document.getElementById('game-container'),
-    scene: null,
-    renderer: null,
-    camera: null,
-    game_configuration: {
-        percentageDonuts: 0,
-        gravity: 0
-    }
+    container: document.getElementById('game-container'),
+    percentageDonuts: 0,
+    gravity: 0
 };
 
 export let setConfiguration = function() {
     
-    init();
-
     const level = prompt('Choose level... easy, medium or hard', 'easy');
     switch(level) {
         case 'easy':
-            configuration.game_configuration.percentageDonuts = 0.2;  
-            configuration.game_configuration.gravity = -0.02;
+            configuration.percentageDonuts = 0.2;  
+            configuration.gravity = -0.02;
             return configuration;
         case 'medium':
-            configuration.game_configuration.percentageDonuts = 0.3;  
-            configuration.game_configuration.gravity = -0.03;
+            configuration.percentageDonuts = 0.3;  
+            configuration.gravity = -0.03;
             return configuration;
         case 'hard':
-            configuration.game_configuration.percentageDonuts = 0.5; 
-            configuration.game_configuration.gravity = -0.05;
+            configuration.percentageDonuts = 0.5; 
+            configuration.gravity = -0.05;
             return configuration;
 
         default:
             console.log('something went wrong...');
             return null;
     }
-};
-
-let init = function() {
-    //create the scene
-    configuration.scene = new THREE.Scene();
-    configuration.scene.background = new THREE.Color(0xabffab);
-    //create and locate camera
-    configuration.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000);
-    configuration.camera.position.z = 7;
-    //create the renderer 
-    configuration.renderer = new THREE.WebGLRenderer();
-    configuration.renderer.setSize(configuration.container.offsetWidth, configuration.container.offsetHeight);
 };
